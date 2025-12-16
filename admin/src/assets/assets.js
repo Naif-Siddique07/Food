806cbd916a0a13acc170ac7500274e71fd7828fe
Naif-Siddique07 <anssiddique0786@gmail.com>
2025-12-16 21:31:53 +0530

    refactor: update asset URL to use production backend and comment out local URL

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -16,5 +16,5 @@ export const assets ={
     header_img
 }
 
-export const url = 'http://localhost:4000'
-// export const url = 'https://food-backend-cc8o.onrender.com'
+// export const url = 'http://localhost:4000'
+export const url = 'https://food-backend-cc8o.onrender.com'
